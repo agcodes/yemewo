a3bb8dba77e6e907329b00c172313a196f805097
agcodes <7748175+agcodes@users.noreply.github.com>
2026-07-04 18:26:51 +0200

    Update FlagsQuizView.vue

--- src/views/FlagsQuizView.vue
+++ src/views/FlagsQuizView.vue
@@ -1,17 +1,6 @@
 <template>
     <FlagsQuiz />
-   <div class="d-flex justify-content-center gap-3">
-        <router-link :to="{ name: 'flags-quiz' }" class="text-decoration-none">
-            <div class="btn-expressive border-gray ">
-                Mode 4 drapeaux
-            </div>
-        </router-link>
-        <router-link :to="{ name: 'guess-flag' }" class="text-decoration-none">  
-            <div class="btn-expressive">
-                Mode 1 drapeau
-            </div>
-        </router-link>
-    </div>
+   
 </template>
 
 <script setup lang="ts">
