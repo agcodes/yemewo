7ad47cca22c3d32162dc9e7d42cbfe23cad03c38
agcodes <7748175+agcodes@users.noreply.github.com>
2026-08-09 20:04:08 +0200

    Update guessWordGame.ts

--- src/stores/guessWordGame.ts
+++ src/stores/guessWordGame.ts
@@ -57,15 +57,15 @@ export const useGameStore = defineStore('guessWordGame', () => {
       isLoading.value = true
       const response = await getSynonym(wordToGuess.value)
       if (response.success && response.synonyms.length > 0) {
-        message.value = `Synonymes : ${response.synonyms.join(', ')}`
+        message.value = `Synonyme : ${response.synonyms[0]}`
         typeAlert.value = 'info'
       } else {
         message.value = 'Aucun synonyme trouvé'
         typeAlert.value = 'warning'
       }
     } catch (error) {
-      message.value = "Erreur lors de la récupération des synonymes"
-      typeAlert.value = 'danger'
+      message.value = "Erreur lors de la récupération du synonyme"
+      typeAlert.value = 'warning'
       console.error('Error fetching synonym:', error)
     } finally {
       isLoading.value = false
